f240337955163fd60cc6f000a3f772db956944eb
shibamoto <silverbirder@gmail.com>
2020-06-06 14:20:03 +0900

    😙 Get User and Create Meeting

--- src/zoom/zoomImpl.ts
+++ src/zoom/zoomImpl.ts
@@ -1,7 +1,5 @@
 'use strict';
 
-import URLFetchRequestOptions = GoogleAppsScript.URL_Fetch.URLFetchRequestOptions;
-
 class ZoomImpl {
     apiKey: string;
     apiSecret: string;
@@ -14,36 +12,38 @@ class ZoomImpl {
     }
 
     updateToken() {
-        const header = Utilities.base64Encode(JSON.stringify({
+        const encodeText = `${Utilities.base64Encode(JSON.stringify({
             'alg': 'HS256',
             'typ': 'JWT'
-        }));
-        const claimSet = JSON.stringify({
+        }))}.${Utilities.base64Encode(JSON.stringify({
             "iss": this.apiKey,
             "exp": Date.now() + 3600
-        });
-        const encodeText = header + "." + Utilities.base64Encode(claimSet);
-        const signature = Utilities.computeHmacSha256Signature(encodeText, this.apiSecret);
-        const jwtToken = encodeText + "." + Utilities.base64Encode(signature);
-        this.token = jwtToken;
+        }))}`;
+        this.token = encodeText + "." + Utilities.base64Encode(Utilities.computeHmacSha256Signature(encodeText, this.apiSecret));
     }
 
     getMeeting(userId: string, startTime: string): any {
-        const data = {
-            'type': 2,
-            'start_time': startTime,
-            'duration': 60,
-            'timezone': 'Asia/Tokyo'
-        };
-        const options: URLFetchRequestOptions = {
+        const response = UrlFetchApp.fetch(`https://api.zoom.us/v2/users/${userId}/meetings`, {
             'method': 'post',
             'contentType': 'application/json',
             'headers': {'Authorization': 'Bearer ' + this.token},
-            'payload': JSON.stringify(data)
-        };
-        const response = UrlFetchApp.fetch('https://api.zoom.us/v2/users/' + userId + '/meetings', options);
-        var cont = JSON.parse(response.getContentText('UTF-8'));
-        return cont;
+            'payload': JSON.stringify({
+                'type': 2,
+                'start_time': startTime,
+                'duration': 60,
+                'timezone': 'Asia/Tokyo'
+            })
+        });
+        return JSON.parse(response.getContentText('UTF-8'));
+    }
+
+    getUsers() {
+        const response = UrlFetchApp.fetch(`https://api.zoom.us/v2/users/`, {
+            'method': 'get',
+            'contentType': 'application/json',
+            'headers': {'Authorization': 'Bearer ' + this.token},
+        });
+        return JSON.parse(response.getContentText('UTF-8'));
     }
 }
 
